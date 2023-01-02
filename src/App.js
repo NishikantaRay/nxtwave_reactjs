ac8ec64c7f889aab8469ed170896fa6d5181f46d
NishikantaRay <nishikantaray1@gmail.com>
2023-01-03 01:32:35 +0530

    Feature:Added login page and connect with redux to store login data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import LoginForm from './components/forms/LoginForm';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div >
       <NavBar/>
-      
+      <LoginForm/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <Provider store={store}> <App /></Provider>
+   
   </React.StrictMode>
 );
 
