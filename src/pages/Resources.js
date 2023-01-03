935600dea5e21999379d32db57344697967696b1
NishikantaRay <nishikantaray1@gmail.com>
2023-01-04 01:53:36 +0530

    Feature:Added loader

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -6,9 +6,12 @@ import axios from 'axios';
 import { Container } from 'react-bootstrap';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
-//import components
-import ItemCard from '../components/ItemCard';
+import { LoadingOverlay, Loader } from 'react-overlay-loader';
+ 
+import 'react-overlay-loader/styles.css';
 
 function Resources() {
   //statate for card data
@@ -16,6 +19,7 @@ function Resources() {
   const [activeKey, setActiveKey] = useState('resources');
   const [searchData, setSearchData] = useState([])
   const [searchInput, setSearchInput] = useState('');
+  const [loading, setLoading] = useState(false);
 
   //api call to get the data
   useEffect(() => {
@@ -36,22 +40,26 @@ function Resources() {
 
   //filter the data based on the active key
   const dataFilter = () => {
+    setLoading(true)
     if (activeKey === 'resources') {
       const data = cardData.filter((element) => {
         return element.tag === 'resource'
       })
+      setInterval(() => setLoading(false), 2000);
       setSearchData(data)
     }
     else if (activeKey === 'request') {
       const data = cardData.filter((element) => {
         return element.tag === 'request'
       })
+      setInterval(() => setLoading(false), 2000);
       setSearchData(data)
     }
     else if (activeKey === 'users') {
       const data = cardData.filter((element) => {
         return element.tag === 'user'
       })
+      setInterval(() => setLoading(false), 2000);
       setSearchData(data)
     } else {
       setSearchData(cardData)
@@ -65,10 +73,12 @@ function Resources() {
   //search function
   const searchItems = (searchValue) => {
     setSearchInput(searchValue)
+    setLoading(true)
     if (searchInput !== "") {
       const data = cardData.filter((item) => {
         return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
       })
+      setInterval(() => setLoading(false), 3000);
       setSearchData(data)
     }
     else {
@@ -102,9 +112,44 @@ function Resources() {
               searchItems(e.target.value);
             }}
           /></div>
-        <div className='container'>
-          <ItemCard searchData={searchData} />
+          <LoadingOverlay > 
+          <div className='container'>
+        <div className='row'>
+          
+          {searchData.map((element) => {
+                        return (
+                            <div className='col col-sm-6 col-md-4 col-lg-4' >
+                                <div id="divLogin" className="m-3 bgImage panel-body-appointment1 d-flex justify-content-center">
+                                    <div className="m-2" style={{ width: "25rem", height: "15 rem" }} >
+                                        <Link className='text-decoration-none text-dark'>
+                                            <Card style={{ width: '100%', height: '15rem', backgroundColor: "aliceblue" }}>
+                                                <Card.Body >
+                                                    <Card.Title>
+                                                        <img src={element.icon_url} width={50} height={50} className="mr-5 mx-auto card-img-sm text-right" alt="Request icon" />
+                                                        &nbsp;&nbsp;&nbsp;&nbsp;
+                                                        {element.title}
+                                                    </Card.Title>
+                                                    <Card.Text className='mt-4'>
+                                                        <a target="_blank" href="{element.link}" className='text-decoration-none'>{element.link}</a>
+                                                    </Card.Text>
+                                                    <Card.Text className='mt-1'>
+                                                        {element.description}
+                                                    </Card.Text>
+                                                </Card.Body>
+                                            </Card>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+
+                        )
+                    })}
+
+            </div>
         </div>
+            <Loader loading={loading}/>
+        </LoadingOverlay>
+        
       </Container>
 
     </>
